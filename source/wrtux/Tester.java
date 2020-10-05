6e0460650ee15974cafdb955da66abdbc9710013
WRtux <wildernessrgr@aliyun.com>
2020-10-05 23:45:26 +0800

    Update step 1
    Optimize Tester logic.

--- source/wrtux/Tester.java
+++ source/wrtux/Tester.java
@@ -8,11 +8,23 @@ final class Tester {
 	
 	static abstract class Task<T> implements Runnable {
 		
+		public static long multiTask(Task<?>[] tsks) {
+			int tcnt = Thread.activeCount();
+			for(Task<?> tsk : tsks)
+				new Thread(tsk).start();
+			while(Thread.activeCount() > tcnt)
+				Thread.yield();
+			long max = 0;
+			for(Task<?> tsk : tsks) if(tsk.time > max)
+				max = tsk.time;
+			return max;
+		}
+		
 		protected final T object;
 		
 		protected long time = -1;
 		
-		protected Task(T o) {
+		public Task(T o) {
 			this.object = o;
 		}
 		
@@ -111,33 +123,40 @@ final class Tester {
 		
 	}
 	
-	/** 尝试让JIT本地化{@link HashtableSimple}。 */
+	/** 尝试让JIT本地化用到的方法。 */
 	static void init() {
-		HashtableSimple htbl = new HashtableSimple(256);
-		for(int i = 0; i < 16384; i++) {
-			String key = Integer.toString(i), val = Integer.toHexString(i);
-			htbl.add(key, val);
-			htbl.put(key, val);
-			htbl.get(key);
+		final int cap = 16384, cnt = 32768;
+		HashtableSimple htbl = new HashtableSimple(cap);
+		Hashtable<String, String> ref = new Hashtable<>(cap);
+		for(int i = 0; i < 16; i++) {
+			for(int j = 0; j < cnt; j++) {
+				String key = Integer.toString(j), val = Integer.toHexString(j);
+				htbl.add(key, val);
+				htbl.put(key, val);
+				htbl.get(key);
+				ref.put(key, val);
+				ref.get(key);
+			}
+			htbl.clear();
+			ref.clear();
 		}
-		htbl.clear();
-		System.gc();
 	}
 	
-	public static void main(String[] args) {
+	static void test(int cap, int cnt) {
+		
+		System.out.printf("=====Test %dc %dn=====%n", cap, cnt);
 		
 		//初始化
-		HashtableSimple htbl = new HashtableSimple(4096);
-		Hashtable<String, String> ref = new Hashtable<>(4096);
-		String[] keys = new String[16384], vals = new String[keys.length];
-		Random rand = new Random();
+		HashtableSimple htbl = new HashtableSimple(cap);
+		Hashtable<String, String> ref = new Hashtable<>(cap);
+		String[] keys = new String[cnt], vals = new String[cnt];
+		final Random rand = new Random();
 		for(int i = 0; i < keys.length; i++) {
 			keys[i] = "k" + i;
 			vals[i] = Integer.toHexString(rand.nextInt());
 		}
-		init();
 		
-		final int tcnt = 4, tnum = keys.length / tcnt;
+		final int tcnt = 4, tnum = cnt / tcnt;
 		String[][] tkeys = new String[tcnt][], tvals = new String[tcnt][];
 		for(int i = 0; i < tkeys.length; i++) {
 			tkeys[i] = Arrays.copyOfRange(keys, tnum * i, tnum * (i + 1));
@@ -170,74 +189,39 @@ final class Tester {
 		HGetter hgtr = new HGetter(ref, keys);
 		hgtr.run();
 		System.out.printf("1T get time ref: %dus%n", hgtr.time / 1000);
+		
+		t1 = System.nanoTime();
+		ref.clear();
+		t2 = System.nanoTime();
+		System.out.printf("Clear time ref: %dus%n", (t2 - t1) / 1000);
 		System.out.println();
 		
 		//HashtableSimple多线程效率测试
 		Adder[] adrs = new Adder[tcnt];
 		for(int i = 0; i < adrs.length; i++)
 			adrs[i] = new Adder(htbl, tkeys[i], tvals[i]);
-		for(Adder i : adrs)
-			new Thread(i).start();
-		while(Thread.activeCount() > 1)
-			Thread.yield();
-		long max = 0;
-		for(Adder i : adrs)
-			if(i.time > max)
-				max = i.time;
-		System.out.printf("4T add time: %dus%n", max / 1000);
+		System.out.printf("4T add time: %dus%n", Task.multiTask(adrs) / 1000);
 		
 		Putter[] ptrs = new Putter[tcnt];
 		for(int i = 0; i < ptrs.length; i++)
 			ptrs[i] = new Putter(htbl, tkeys[i], tvals[i]);
-		for(Putter i : ptrs)
-			new Thread(i).start();
-		while(Thread.activeCount() > 1)
-			Thread.yield();
-		max = 0;
-		for(Putter i : ptrs)
-			if(i.time > max)
-				max = i.time;
-		System.out.printf("4T put time: %dus%n", max / 1000);
+		System.out.printf("4T put time: %dus%n", Task.multiTask(ptrs) / 1000);
 		
 		Getter[] gtrs = new Getter[tcnt];
 		for(int i = 0; i < gtrs.length; i++)
 			gtrs[i] = new Getter(htbl, tkeys[i]);
-		for(Getter i : gtrs)
-			new Thread(i).start();
-		while(Thread.activeCount() > 1)
-			Thread.yield();
-		max = 0;
-		for(Getter i : gtrs)
-			if(i.time > max)
-				max = i.time;
-		System.out.printf("4T get time: %dus%n", max / 1000);
+		System.out.printf("4T get time: %dus%n", Task.multiTask(gtrs) / 1000);
 		
 		//API Hashtable多线程效率测试
 		HPutter[] hptrs = new HPutter[tcnt];
 		for(int i = 0; i < hptrs.length; i++)
 			hptrs[i] = new HPutter(ref, tkeys[i], tvals[i]);
-		for(HPutter i : hptrs)
-			new Thread(i).start();
-		while(Thread.activeCount() > 1)
-			Thread.yield();
-		max = 0;
-		for(HPutter i : hptrs)
-			if(i.time > max)
-				max = i.time;
-		System.out.printf("4T put time ref: %dus%n", max / 1000);
+		System.out.printf("4T put time ref: %dus%n", Task.multiTask(hptrs) / 1000);
 		
 		HGetter[] hgtrs = new HGetter[tcnt];
 		for(int i = 0; i < hgtrs.length; i++)
 			hgtrs[i] = new HGetter(ref, tkeys[i]);
-		for(HGetter i : hgtrs)
-			new Thread(i).start();
-		while(Thread.activeCount() > 1)
-			Thread.yield();
-		max = 0;
-		for(HGetter i : hgtrs)
-			if(i.time > max)
-				max = i.time;
-		System.out.printf("4T get time ref: %dus%n", max / 1000);
+		System.out.printf("4T get time ref: %dus%n", Task.multiTask(hgtrs) / 1000);
 		
 		//数据验证
 		System.out.println();
@@ -245,8 +229,14 @@ final class Tester {
 			int j = i - 1;
 			System.out.printf("Key<%s>: %s - %s%n", keys[j], htbl.get(keys[j]), vals[j]);
 		}
-		System.out.printf("Capacity: %d, size: %d%n", htbl.getCapacity(), htbl.getSize());
+		System.out.println();
 		
+	}
+	
+	public static void main(String[] args) {
+		init();
+		test(16384, 32768);
+		test(65536, 131072);
 	}
 	
 }
